944d120ddf66d8ec99ee83d170ca95224ed36bbb
mikan <mikanpk@users.noreply.github.com>
2025-08-02 21:50:09 +0900

    add: 簡単起動用bat

--- 起動.bat
+++ 起動.bat
@@ -1,6 +1,6 @@
 uv sync
 REM callを使って仮想環境をアクティベート
-call .\.venv\Scripts\activate.bat 
-python SerialController/Window.py
+call ".venv\Scripts\activate.bat"
+python "SerialController/Window.py"
 REM 仮想環境の停止
 deactivate
